6776f7b6a758f93566549c903507c7c53762aeb6
Gadiraju Tejaswi <102372836+Tej2717@users.noreply.github.com>
2022-11-27 11:30:54 +0530

    java

---
+++ Java/SwitchExample.java
@@ -0,0 +1,86 @@
+package loopAndFlow;
+
+public class SwitchExamples {
+	public static void main(String[] args) {
+		int number = 2;
+		switch (number) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);
+			break;
+		default:
+			System.out.println("Default");
+			break;
+		}
+
+	
+		number = 2;
+		switch (number) {
+		case 1:
+			System.out.println(1);
+		case 2:
+			System.out.println(2);
+		case 3:
+			System.out.println(3);
+		default:
+			System.out.println("Default");
+		}
+		
+		number = 2;
+		switch (number) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+		case 3:
+			System.out.println("Number is 2 or 3");
+			break;
+		default:
+			System.out.println("Default");
+			break;
+		}
+		
+		number = 10;
+		switch (number) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);
+			break;
+		default:
+			System.out.println("Default");
+			break;
+		}
+		
+		number = 10;
+		switch (number) {
+		default:
+			System.out.println("Default");
+			break;
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);
+			break;
+		}
+		
+		number = 10;
+		switch (number) {
+	
+		}
+
+	}
+}
